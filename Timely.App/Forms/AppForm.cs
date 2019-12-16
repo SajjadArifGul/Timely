325fff85cf1434263ae12cbd4e581b6a0506263f
Sajjad Gul <sajjadarifgul@gmail.com>
2019-12-16 17:52:08 +0500

    Initial

--- Timely.App/Forms/AppForm.cs
+++ Timely.App/Forms/AppForm.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Timely.App
+namespace Timely.App.Forms
 {
-    public partial class Form1 : Form
+    public partial class AppForm : Form
     {
-        public Form1()
+        public AppForm()
         {
             InitializeComponent();
         }
--- Timely.App/Program.cs
+++ Timely.App/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Timely.App.Forms;
 
 namespace Timely.App
 {
@@ -16,7 +17,7 @@ namespace Timely.App
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AppForm());
         }
     }
 }
