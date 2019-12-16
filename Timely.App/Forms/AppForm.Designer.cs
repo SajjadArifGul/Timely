b70166ea4062c0458237bc08f0b4d8fb7dd308a0
Sajjad Gul <sajjadarifgul@gmail.com>
2019-12-16 18:22:50 +0500

    Stop Watch and Timespan implementation

--- Timely.App/Forms/AppForm.Designer.cs
+++ Timely.App/Forms/AppForm.Designer.cs
@@ -28,21 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // label1
+            // lblTime
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.lblTime.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(20);
-            this.label1.Size = new System.Drawing.Size(414, 71);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "00 : 00 : 00 ";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTime.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(12, 9);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Padding = new System.Windows.Forms.Padding(20);
+            this.lblTime.Size = new System.Drawing.Size(414, 71);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "00 : 00 : 00 : 00";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(180, 123);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // AppForm
             // 
@@ -50,7 +68,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(438, 204);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblTime);
             this.Name = "AppForm";
             this.Text = "Timely";
             this.ResumeLayout(false);
@@ -59,6 +78,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Timely.App/Forms/AppForm.cs
+++ Timely.App/Forms/AppForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,25 @@ namespace Timely.App.Forms
 {
     public partial class AppForm : Form
     {
+        Stopwatch sw = Stopwatch.StartNew();
+
         public AppForm()
         {
             InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var ts = TimeSpan.FromTicks(sw.ElapsedTicks);
+            //lblTime.Text = string.Format("{0} : {1} : {2} : {3}", ts.Hours.ToString("00"), ts.Minutes.ToString("00"), ts.Seconds.ToString("00"), ts.Milliseconds.ToString("00"));
+            lblTime.Text = string.Format("{0} : {1} : {2} : {3}", ts.ToString("hh"), ts.ToString("mm"), ts.ToString("ss"), ts.ToString("ff"));
+
+            //lblTime.Text = ts.ToString("hh\\:mm\\:ss\\:ff");
         }
     }
 }
